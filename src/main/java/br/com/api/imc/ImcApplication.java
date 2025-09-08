de8d623f07438d1d33d277d1f857235cd855a95c
Tiago <tiago.polemarxgroup@gmail.com>
2025-09-08 20:22:23 -0300

    projeto criado

---
+++ src/main/java/br/com/api/imc/ImcApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.imc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImcApplication.class, args);
+    }
+
+}
